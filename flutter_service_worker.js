dee3c28ee558a701f752f7716add2c7248297e6c
sheldonlidev <252885626+sheldonlidev@users.noreply.github.com>
2026-01-14 18:51:34 +0000

    Deploying to edgeone-release from @ open-guji/guji-toolkit-web@e392739ae65c9a9f505f2d54c5c299105a7fa235 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "32d67cefa8afc63db80af7e14345e042",
-"flutter_bootstrap.js": "fe30e9a961e08f1e20b6ca8c62601024",
+"flutter_bootstrap.js": "81a36d5f85de3335ce0670c38cde0edf",
 "version.json": "4a5e4fec9723b3ecd6701268aeea9f74",
 "main.dart.js": "40c0e2914036ede199f8df047fdd9e4c"};
 // The application shell files that are downloaded before a service worker can
